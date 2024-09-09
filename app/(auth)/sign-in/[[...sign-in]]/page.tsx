edf199cd85870f49fb0f5418226e2fbf35c069af
rohit <rohitsirsat855@gmail.com>
2024-09-09 21:40:09 +0530

    change the ui

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      {/* Clerk SignIn Component */}
+      <SignIn />
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      {/* Clerk SignUp Component */}
+      <SignUp />
+    </div>
+  );
 }
